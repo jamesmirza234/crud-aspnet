d0c16e3718142ce351fd887f6e944a3e4450ecb4
Minda <minda.emih@mncgroup.com>
2022-11-29 17:52:27 +0700

    first commit

---
+++ ADO Example/Controllers/ProductController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ADO_Example.DAL;
+using ADO_Example.Models;
+
+namespace ADO_Example.Controllers
+{
+    public class ProductController : Controller
+    {
+        Product_DAL _productDAL = new Product_DAL();
+        // GET: Product
+        public ActionResult Index()
+        {
+            var productList = _productDAL.GetAllProducts();
+            if (productList.Count == 0)
+            {
+                TempData["InfoMessage"] = "Currently products not avalaible";
+            }
+            return View(productList);
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            bool IsInserted = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    IsInserted = _productDAL.InsertProduct(product);
+                    if (IsInserted)
+                    {
+                        TempData["MessageSuccess"] = "Data Saved Succesfully";
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = "Data Unable Saved";
+                    }
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+            return View();
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ADO Example/DAL/Product_DAL.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using ADO_Example.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ADO_Example.DAL
+{
+    public class Product_DAL
+    {
+        string conString = ConfigurationManager.ConnectionStrings["adoConnectionString"].ToString();
+
+        public List<Product> GetAllProducts()
+        {
+            List<Product> productlist = new List<Product>();
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "sp_GetAllProducts";
+                SqlDataAdapter sqlDA = new SqlDataAdapter(command);
+                DataTable dtProducts = new DataTable();
+                connection.Open();
+                sqlDA.Fill(dtProducts);
+                connection.Close();
+                foreach (DataRow dr in dtProducts.Rows)
+                {
+                    productlist.Add(new Product
+                    {
+                        ProductID = Convert.ToInt32(dr["ProductID"]),
+                        ProductName = dr["ProductName"].ToString(),
+                        Price = Convert.ToDecimal(dr["Price"]),
+                        Qty = Convert.ToInt32(dr["Qty"]),
+                        Remarks = dr["Remarks"].ToString()
+                    });
+                }
+                
+            }
+                return productlist;
+        }
+
+        public bool InsertProduct(Product product)
+        {
+            int id = 0;
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = new SqlCommand("sp_InsertProducts",connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@ProductName", product.ProductName);
+                command.Parameters.AddWithValue("@Price", product.Price);
+                command.Parameters.AddWithValue("@Qty", product.Qty);
+                command.Parameters.AddWithValue("@Remarks", product.Remarks);
+                connection.Open();
+                id = command.ExecuteNonQuery();
+                connection.Close();
+                
+            }
+            if (id > 0 )
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+        }
+    }
+}
